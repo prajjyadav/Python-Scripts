7e95679b59844a192c9e499f5bffbd05ebb0fe5f
Shloka gupta <chicken-biryani@users.noreply.github.com>
2020-10-02 01:47:38 +0530

    Whatsapp Message
    Sends a whatsapp message to the number at a  paticular time.
    
    I've given a random Number here

---
+++ WhatsappMessage.py
@@ -0,0 +1,18 @@
+import pywhatkit as kit
+kit.sendwhatmsg("+918432421981","i think u are awesome",18,16)
+
+'''this code sends a whatsapp message to a the number given 
+at the paticular time given after that
+
+over here:
+
+kit.sendwhatmsg("+918432421981", <---- the number (i've put a random number here)
+"i think u are awesome",<--- the message
+18,16) <---the time in hours and minutes
+
+
+Conditions:
+	1)pip install pywhatkit
+	2)keep your whatsapp web logged in and the internet working
+	3)keep the time between, when you run the code and the time you set more than a minute or two
+'''
